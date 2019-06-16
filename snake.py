44069d525c7d822aea8d8c3311b5f8d33c0aaf82
iknowright <changchaishi@gmail.com>
2019-06-16 10:55:05 +0800

    code refraction

--- snake.py
+++ snake.py
@@ -32,78 +32,58 @@ def default_setting():
 default_setting()
 
 while game:
+    # Button A make a left turn, Butten B make a right turn
     if button_a.was_pressed():
         current_direction += 1
     elif button_b.was_pressed():
         current_direction -= 1
 
+    # Rendering the map per delay (eg per 0.05)
     map_arr = blank_map.copy()
-    map_arr[food[1]*5 + food[0]] = "9"
+    map_arr[food[1]*5 + food[0]] = "5"
     for body in snake_body:
         map_arr[body[1]*5 + body[0]] = "9"
     the_map = "".join(map_arr)
     the_map = the_map[:5] + ':' + the_map[5:10] + ':' + the_map[10:15] + ':' + the_map[15:20] + ':' + the_map[20:] 
     the_map = Image(the_map)
     display.show(the_map)
+
+    # Take the direction action and update the snake body (eg per 0.5 sec)
     if counter % 10 == 0:
-        if current_direction % 4 == 0: # up
-            for i in reversed(range(len(snake_body))):
-                if i == len(snake_body) - 1:
-                    tail = snake_body[i]
-                if i == 0:
+        for i in reversed(range(len(snake_body))):
+            if i == len(snake_body) - 1:
+                tail = snake_body[i]
+                snake_body[i] = snake_body[i-1].copy()
+            elif i == 0:
+                if current_direction % 4 == 0:
                     if snake_body[i][1] - 1 < 0:
                         game = False
                         break
-                    snake_body[i][1] = snake_body[i][1] - 1
-                    print(snake_body[i])
-                    if snake_body[i] == food:
-                        snake_body.append(tail)
-                        generate_food()
-                else:
-                    snake_body[i] = snake_body[i-1].copy()
-                    print(snake_body[i])
-        elif current_direction % 4 == 1: # left
-            for i in reversed(range(len(snake_body))):
-                if i == len(snake_body) - 1:
-                    tail = snake_body[i]
-                if i == 0:
+                    else:
+                        snake_body[i][1] = snake_body[i][1] - 1
+                elif current_direction % 4 == 1:
                     if snake_body[i][0] - 1 < 0:
                         game = False
                         break
-                    snake_body[i][0] = snake_body[i][0] - 1
-                    if snake_body[i] == food:
-                        snake_body.append(tail)
-                        generate_food()
-                else:
-                    snake_body[i] = snake_body[i-1].copy()
-        elif current_direction % 4 == 2: # down  
-            for i in reversed(range(len(snake_body))):
-                if i == len(snake_body) - 1:
-                    tail = snake_body[i]
-                if i == 0:
+                    else:
+                        snake_body[i][0] = snake_body[i][0] - 1
+                elif current_direction % 4 == 2:                        
                     if snake_body[i][1] + 1 >= 5:
                         game = False
                         break
-                    snake_body[i][1] = snake_body[i][1] + 1
-                    if snake_body[i] == food:
-                        snake_body.append(tail)
-                        generate_food()
-                else:
-                    snake_body[i] = snake_body[i-1].copy()
-        elif current_direction % 4 == 3: # right
-            for i in reversed(range(len(snake_body))):
-                if i == len(snake_body) - 1:
-                    tail = snake_body[i]
-                if i == 0:
+                    else:
+                        snake_body[i][1] = snake_body[i][1] + 1
+                elif current_direction % 4 == 3:                
                     if snake_body[i][0] + 1 >= 5:
                         game = False
                         break
-                    snake_body[i][0] = snake_body[i][0] + 1
-                    if snake_body[i] == food:
-                        snake_body.append(tail)
-                        generate_food()
-                else:
-                    snake_body[i] = snake_body[i-1].copy()
+                    else:
+                        snake_body[i][0] = snake_body[i][0] + 1             
+                if snake_body[i] == food:
+                    snake_body.append(tail)
+                    generate_food()
+            else:
+                snake_body[i] = snake_body[i-1].copy()
 
     time.sleep(0.05)
     counter += 1
