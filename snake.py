32401796a6b60668c5c42d3d365884bd318422b4
iknowright <changchaishi@gmail.com>
2019-06-16 11:29:29 +0800

    snake self die

--- snake.py
+++ snake.py
@@ -13,7 +13,6 @@ current_direction = 0
 counter = 0
 game = True
 
-
 def generate_food():
     got_food = False
     while not got_food:
@@ -30,60 +29,54 @@ def default_setting():
     counter = 0
 
 default_setting()
-
 while game:
-    # Button A make a left turn, Butten B make a right turn
-    if button_a.was_pressed():
-        current_direction += 1
-    elif button_b.was_pressed():
-        current_direction -= 1
+# Button A make a left turn, Butten B make a right turn
+if button_a.was_pressed():
+    current_direction += 1
+elif button_b.was_pressed():
+    current_direction -= 1
 
-    # Rendering the map per delay (eg per 0.05)
-    map_arr = blank_map.copy()
-    map_arr[food[1]*5 + food[0]] = "5"
-    for body in snake_body:
-        map_arr[body[1]*5 + body[0]] = "9"
-    the_map = "".join(map_arr)
-    the_map = the_map[:5] + ':' + the_map[5:10] + ':' + the_map[10:15] + ':' + the_map[15:20] + ':' + the_map[20:] 
-    the_map = Image(the_map)
-    display.show(the_map)
+# Rendering the map per delay (eg per 0.05)
+map_arr = blank_map.copy()
+map_arr[food[1]*5 + food[0]] = "5"
+for body in snake_body:
+    map_arr[body[1]*5 + body[0]] = "9"
+the_map = "".join(map_arr)
+the_map = the_map[:5] + ':' + the_map[5:10] + ':' + the_map[10:15] + ':' + the_map[15:20] + ':' + the_map[20:] 
+the_map = Image(the_map)
+display.show(the_map)
 
-    # Take the direction action and update the snake body (eg per 0.5 sec)
-    if counter % 10 == 0:
-        for i in reversed(range(len(snake_body))):
-            if i == len(snake_body) - 1:
-                tail = snake_body[i]
-                snake_body[i] = snake_body[i-1].copy()
-            elif i == 0:
-                if current_direction % 4 == 0:
-                    if snake_body[i][1] - 1 < 0:
-                        game = False
-                        break
-                    else:
-                        snake_body[i][1] = snake_body[i][1] - 1
-                elif current_direction % 4 == 1:
-                    if snake_body[i][0] - 1 < 0:
-                        game = False
-                        break
-                    else:
-                        snake_body[i][0] = snake_body[i][0] - 1
-                elif current_direction % 4 == 2:                        
-                    if snake_body[i][1] + 1 >= 5:
-                        game = False
-                        break
-                    else:
-                        snake_body[i][1] = snake_body[i][1] + 1
-                elif current_direction % 4 == 3:                
-                    if snake_body[i][0] + 1 >= 5:
-                        game = False
-                        break
-                    else:
-                        snake_body[i][0] = snake_body[i][0] + 1             
-                if snake_body[i] == food:
-                    snake_body.append(tail)
-                    generate_food()
-            else:
-                snake_body[i] = snake_body[i-1].copy()
-
-    time.sleep(0.05)
-    counter += 1
+# Take the direction action and update the snake body (eg per 0.5 sec)
+if counter % 10 == 0:
+    for i in reversed(range(len(snake_body))):
+        if i == len(snake_body) - 1:
+            tail = snake_body[i]
+            snake_body[i] = snake_body[i-1].copy()
+        elif i == 0:
+            if current_direction % 4 == 0:
+                snake_body[i][1] = snake_body[i][1] - 1
+                if snake_body[i][1] < 0 or snake_body.count(snake_body[i]) >= 2:
+                    game = False
+                    break
+            elif current_direction % 4 == 1:
+                snake_body[i][0] = snake_body[i][0] - 1
+                if snake_body[i][0] < 0 or snake_body.count(snake_body[i]) >= 2:
+                    game = False
+                    break
+            elif current_direction % 4 == 2:                        
+                snake_body[i][1] = snake_body[i][1] + 1
+                if snake_body[i][1] >= 5 or snake_body.count(snake_body[i]) >= 2:
+                    game = False
+                    break
+            elif current_direction % 4 == 3:                
+                snake_body[i][0] = snake_body[i][0] + 1             
+                if snake_body[i][0] >= 5 or snake_body.count(snake_body[i]) >= 2:
+                    game = False
+                    break
+            if snake_body[i] == food:
+                snake_body.append(tail)
+                generate_food()
+        else:
+            snake_body[i] = snake_body[i-1].copy()
+time.sleep(0.05)
+counter += 1
